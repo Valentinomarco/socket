3561482a0730d345eb4b3c1855914f9e4e11bd0c
Valentinomarco <valentinomarco2002@gmail.com>
2021-03-31 11:58:41 +0200

    ciao

--- client.py
+++ client.py
@@ -2,18 +2,36 @@
 
 import socket
 
-SERVER_ADDRESS = '127.0.0.1'  # The server's hostname or IP address
-SERVER_PORT = 65433        # The port used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((SERVER_ADDRESS, SERVER_PORT))
-dati = input("Inserisci messaggio per il server: ")
-dati = dati.encode()
-# Send data to server
-s.send(dati)
-# Receive response from server
-dati = s.recv(2048)
-if dati:
-    # Convert back to string for python3
-    dati = dati.decode()
-    print("Ho ricevuto dal server: ")
-    print(dati + '\n')
+SERVER_ADDRESS = '127.0.0.1'
+SERVER_PORT = 22224
+
+sock_service = socket.socket()
+
+sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+
+print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
+
+protocollo=["SYN","SYN" + "ACK", "ACK + data", "ACK for Data"]
+step = 0
+dati=str(step)
+
+while True:
+    try:
+        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ") #richiesta di un input 
+    except EOFError:
+        print("\nOkay. Exit")
+        break
+    if not dati:
+        print("Non puoi inviare una stringa vuota!") #controllo
+        continue
+    if dati == '0':
+        print("Chiudo la connessione con il server!") #chiusura del server
+        break
+    
+    dati = dati.encode()
+
+    sock_service.send(dati)
+
+    dati = sock_service.recv(2048)
+
+sock_service.close()
